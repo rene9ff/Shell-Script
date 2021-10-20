9ce50f2e46d1af7f3a32b096bd7a8612a2302ac7
rene9ff <66588109+rene9ff@users.noreply.github.com>
2021-10-20 10:18:24 +0800

    Add files via upload
    查看Linux发行版和内核

---
+++ os.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/bash
+get_opsy() {    # 获取Linux发行版和内核
+    [ -f /etc/redhat-release ] && awk '{print ($1,$3~/^[0-9]/?$3:$4)}' /etc/redhat-release && return
+    [ -f /etc/os-release ] && awk -F'[= "]' '/PRETTY_NAME/{print $3,$4,$5}' /etc/os-release && return
+    [ -f /etc/lsb-release ] && awk -F'[="]+' '/DESCRIPTION/{print $2}' /etc/lsb-release && return
+}
+opsy=$(get_opsy)
+echo ${opsy}" kernel "$(uname -r)
