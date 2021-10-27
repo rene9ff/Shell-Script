51ea5534150d161f412e7d537bd9351a26786e52
rene9ff <66588109+rene9ff@users.noreply.github.com>
2021-10-27 17:31:22 +0800

    Update root.sh

--- root.sh
+++ root.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
-sed 's/PermitRootLogin no/PermitRootLogin yes/' /etc/ssh/sshd_config
-sed 's/PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config
+sed -i 's/PermitRootLogin no/PermitRootLogin yes/' /etc/ssh/sshd_config
+sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config
 service sshd restart
 passwd root
+reboot
